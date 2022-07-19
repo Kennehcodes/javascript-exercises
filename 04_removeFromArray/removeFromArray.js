8efac20b0f5c622759d039f5009420a55a017345
kennehcodes <lee.n.mackenzie@gmail.com>
2022-07-19 14:11:20 -0700

    Add Remove From Array Function
    This function implements use of splice to remove a item from an array, this implementation is in computation time of O(N^2)

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,18 @@
-const removeFromArray = function() {
+//This function works by taking in a user index as the first parameter
+//any subsequent parameters will be items to search for and delete.
+//these items are referenced by their value not their index
 
+const removeFromArray = function(userArr, ...param) {
+
+    for (const pTimes in param) {
+        for (const item in userArr) {
+            if (param[pTimes] === userArr[item]) {
+                userArr.splice(item, 1);
+            }
+    }
+    }
+
+    return userArr;
 };
 
 // Do not edit below this line
