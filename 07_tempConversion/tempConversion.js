1efd6abde333407f1251c92e0feb8851bdac60d0
kennehcodes <lee.n.mackenzie@gmail.com>
2022-07-19 15:36:38 -0700

    C to F and F to C Converter Function
    Implemented temperature converting functions

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,10 +1,21 @@
-const ftoc = function() {
+const ftoc = function(fahTemp) {
+  
+  let conversion = (fahTemp - 32) * 5/9; 
 
+  
+  let result = (Math.round(conversion * 10)) / 10;
+  return result;
 };
 
-const ctof = function() {
-
+const ctof = function(celTemp) {
+  
+  let conversion = ((celTemp) * 9/5 + 32)
+  
+  let result = (Math.round(conversion * 10)) / 10;
+  return result;
 };
+console.log(ftoc(32));
+
 
 // Do not edit below this line
 module.exports = {
