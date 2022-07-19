426b7ce564eee9c74ad6233ffd681a9cde9384e6
kennehcodes <lee.n.mackenzie@gmail.com>
2022-07-19 11:44:08 -0700

    To add reverseString Function

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,10 @@
-const reverseString = function() {
-
+const reverseString = function(inputStr) {
+    let stringBuffer = "";
+    let strLen = inputStr.length - 1;
+    for (; strLen >= 0; strLen--) {
+        stringBuffer += inputStr[strLen];
+    }
+    return stringBuffer;     
 };
 
 // Do not edit below this line
