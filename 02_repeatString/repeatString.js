3678f69bc50f74362e075a8ce686cf2f0f9620e7
kennehcodes <lee.n.mackenzie@gmail.com>
2022-07-19 11:34:16 -0700

    Add Solution to Repeat Strings
    Implented function with control logic to repeat a give string a given amount of times

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,15 @@
-const repeatString = function() {
-
+const repeatString = function(inputStr, repeatTimes) {
+    let holder ="";
+    if (repeatTimes < 0) {
+        return "ERROR";
+    } else if (repeatTimes === 0) {
+        return holder;
+    } else {
+        for (let i = 0; i < repeatTimes; i++) {
+            holder += inputStr;
+        }
+        return holder;
+    }
 };
 
 // Do not edit below this line
