255ac8bab85eb99d450ccc8b94efa7eef4c6f9c1
kennehcodes <lee.n.mackenzie@gmail.com>
2022-08-03 15:58:36 -0700

    Implemented palindromes function

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,20 @@
-const palindromes = function () {
+const palindromes = function (givenString) {
 
+    givenString =  givenString.replace(/[^\w]/g, "").toLowerCase();
+    
+
+    revString = Array.from(givenString);
+    
+    let reversed = revString.reverse();
+
+    reversedRed = reversed.reduce((final, element) =>
+    {
+        return final + element;
+
+    }, "");
+
+
+    return (reversedRed === givenString)
 };
 
 // Do not edit below this line
