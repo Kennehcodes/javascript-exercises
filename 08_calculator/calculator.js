be3ff802f1e20ac0c3d0390b5b638450bd50949f
kennehcodes <lee.n.mackenzie@gmail.com>
2022-08-03 15:20:08 -0700

    Added functionality for the calculator application

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,48 @@
-const add = function() {
+const add = function(a, b) {
+	return a+b;
+};
+
+const subtract = function(a,b) {
+  return a-b;
 	
 };
 
-const subtract = function() {
-	
+const sum = function(arr) {
+
+  if (arr.length === 0) {
+    return 0;
+  } else {
+  let x = arr.reduce((total, element) =>
+  {
+    return total+= element;
+  } )
+  return x;
+}
 };
 
-const sum = function() {
-	
+const multiply = function(arr) {
+  let product = arr.reduce((total, currentNumber) => {
+    return total *= currentNumber;
+  })
+return product;
 };
 
-const multiply = function() {
-
+const power = function(a, b) {
+  return a ** b;
 };
 
-const power = function() {
-	
-};
+const factorial = function(n) {
+  if (n === 0) {
+    return 1;
+  }
 
-const factorial = function() {
-	
+  let accumulator = 1;
+  while (n !== 1) 
+  {
+    accumulator *= n;
+    n--;
+  }
+  return accumulator;
 };
 
 // Do not edit below this line
