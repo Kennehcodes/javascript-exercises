3fb0a3591a8aca72e3ba5ee74d7641092c4c6896
kennehcodes <lee.n.mackenzie@gmail.com>
2022-08-04 00:43:46 -0700

    solution using reduce

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,6 +1,26 @@
-const findTheOldest = function() {
+const findTheOldest = function(personArr) {
+    let toGiveBack = 0;
+    let thisYear = new Date();
+    thisYear = thisYear.getFullYear();
+  
 
-};
+    let findOldest = personArr.reduce((tally, element) => {
+        
+        if (element.yearOfDeath === undefined) {
+            element.yearOfDeath = thisYear;
+        }
+        element.yearsAlive = (element.yearOfDeath - element.yearOfBirth);
+        if (element.yearsAlive > tally.years) {
+            tally.years = element.yearsAlive; 
+            tally.name = element.name;
+        }
+        return tally;
+    }, {years: 0})
 
+
+
+
+return findOldest;
+}
 // Do not edit below this line
 module.exports = findTheOldest;
