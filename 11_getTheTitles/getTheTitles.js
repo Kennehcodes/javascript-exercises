defa88434e6324a31acc8478b9deeb6e78a2b1e5
kennehcodes <lee.n.mackenzie@gmail.com>
2022-08-03 23:37:52 -0700

    Solution for the get the titles exercise

--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -1,6 +1,13 @@
-const getTheTitles = function() {
+const getTheTitles = function(objArr) {
 
-};
+let o = objArr.reduce((tally, item) => {
 
+tally.push(item.title);
+return tally;
+}, [])
+
+return o;
+
+} 
 // Do not edit below this line
 module.exports = getTheTitles;
