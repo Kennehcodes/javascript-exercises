cad47b4394f48a15fdca561d8823199d4fa58ad5
kennehcodes <lee.n.mackenzie@gmail.com>
2022-08-03 17:18:57 -0700

    Added fibonacci maker

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,30 @@
-const fibonacci = function() {
+const fibonacci = function (m) {
+  n = parseInt(m);
 
+  if (n < 0) {
+    return "OOPS";
+  }
+  //the fibonacci sequence works by
+  //shifting down values, similar to bitwise operating
+
+  function makeFib(n) {
+    let a = 1;
+    let b = 0;
+    let sum = 0;
+    let fibArr = [];
+
+    for (let k = 1; k <= n; k++) {
+      sum = a + b;
+      fibArr.push(sum);
+      a = b;
+      b = sum;
+    }
+
+    return fibArr;
+  }
+
+  let findIt = makeFib(n);
+  return findIt[n-1];
 };
 
 // Do not edit below this line
